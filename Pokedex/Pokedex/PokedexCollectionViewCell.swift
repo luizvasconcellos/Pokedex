b53a0c373ef151735207c1b5a9eb6a9301875f04
Luiz Vasconcellos <luizvasconcellos@gmail.com>
2021-04-24 01:23:29 -0300

    Incluir CollectionViewCell na Home

---
+++ Pokedex/Pokedex/PokedexCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PokedexCollectionViewCell.swift
+//  Pokedex
+//
+//  Created by Luiz Vasconcellos on 24/04/21.
+//
+
+import UIKit
+
+class PokedexCollectionViewCell: UICollectionViewCell {
+    
+    func setup() {
+        self.backgroundColor = UIColor.red
+    }
+}
--- Pokedex/Pokedex/ViewController.swift
+++ Pokedex/Pokedex/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pokedexCollectionView: UICollectionView!
+    
+    private let itemsPerRow: CGFloat = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let logo = UIImage(named: "logo-pokemon")
+        let imageView = UIImageView(image:logo)
+        self.navigationItem.titleView = imageView
+        
+        pokedexCollectionView.dataSource = self
+        pokedexCollectionView.delegate = self
     }
 
+}
 
+extension ViewController:UICollectionViewDataSource,UICollectionViewDelegate {
+    
+    //MARK: Collection View
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let pokedexCell = collectionView.dequeueReusableCell(withReuseIdentifier: "pokedexCell", for: indexPath) as! PokedexCollectionViewCell
+        pokedexCell.setup()
+        
+        return pokedexCell
+    }
 }
 
