18e81cfac7a3784c461ea8840cc17ef887ce1487
Luiz Vasconcellos <luizvasconcellos@gmail.com>
2021-04-24 00:25:42 -0300

    Criar projeto com a pod do Alamofire

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Luiz Vasconcellos on 24/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
